b5805ae878491f9d7580fdce582ff29cff9dc040
Kaua_Matheus <kmsilva3@minha.fag.edu.br>
2025-04-21 18:07:24 -0300

    feat: activity using tailwind

---
+++ Atividade 2/src/App.tsx
@@ -0,0 +1,92 @@
+// React
+import { useEffect, useState } from 'react';
+
+// Axios
+import axios from 'axios';
+
+function App() {
+
+  const url = "https://api.github.com/users/Kaua-Matheus";
+
+  const [loaded, setLoaded] = useState(false)
+  const [data, setData] = useState(
+    {
+      perfil: "",
+      nome: "",
+      bio: "",
+      public_repo: "",
+      html_url: "",
+    }
+  )
+
+  interface ApiResponse {
+    perfil: string;
+    nome: string;
+    bio: string;
+    public_repo: string;
+    html_url: string,
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(url);
+      const formattedData: ApiResponse = {
+        perfil: response.data.avatar_url,
+        nome: response.data.name,
+        bio: response.data.bio,
+        public_repo: response.data.repos_url,
+        html_url: response.data.html_url,
+      };
+      console.log(formattedData)
+      setData(formattedData)
+      setLoaded(true)
+    };
+    getData();
+  }, []);
+
+
+  return (
+
+    <div
+      className="flex justify-center items-center h-screen"
+    >
+      {loaded ? 
+      <div
+      className='flex flex-col items-center text-center bg-[#D9D9D9] pt-10 pb-5 px-14 rounded-lg space-y-6'
+    >
+      <div>
+        <img 
+          className="rounded-full h-16 w-16" src={data.perfil} alt="Imagem Exemplo" />
+      </div>
+
+      <div
+        className="flex flex-col space-y-2"
+      >
+        <p>{data.nome}</p>
+        <hr 
+          className="border-black w-full"/>
+        <p>{data.bio}</p>
+        <hr 
+          className="border-black w-full"/>
+        <p>{data.public_repo}</p>
+      </div>
+
+      <div>
+        <a
+          href={data.html_url}
+          target='_blank'
+          className="bg-green-900 px-2 py-1 rounded text-white"
+        >
+          html_url
+        </a>
+      </div>
+    </div>
+     : 
+    <div>
+      Carregando..
+    </div>}
+    </div>
+  )
+}
+
+export default App
---
+++ Atividade 2/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Inclua os caminhos corretos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
